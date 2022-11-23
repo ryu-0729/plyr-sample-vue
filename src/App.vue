c9d393b1cfdd0830cb65defb1166360691755005
CodeSandbox <support@codesandbox.io>
2022-11-23 15:11:11 +0000

    "Initial commit"

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <PlyrSample />
+</template>
+
+<script setup lang="ts">
+import PlyrSample from "./components/PlyrSample.vue";
+</script>
---
+++ src/components/PlyrSample.vue
@@ -0,0 +1,64 @@
+<template>
+  <video id="player" class="video" ref="video">
+    <source
+      src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+      type="video/mp4"
+    />
+  </video>
+</template>
+
+<script>
+import Plyr from "plyr";
+
+export default {
+  name: "PlyrSample",
+  setup() {
+    const player = new Plyr("#player", {
+      debug: true,
+      title: "View From A Blue Moon",
+      iconUrl: "dist/demo.svg",
+      keyboard: {
+        global: true,
+      },
+      tooltips: {
+        controls: true,
+      },
+      captions: {
+        active: true,
+      },
+      vimeo: {
+        // Prevent Vimeo blocking plyr.io demo site
+        referrerPolicy: "no-referrer",
+      },
+      mediaMetadata: {
+        title: "View From A Blue Moon",
+        album: "Sports",
+        artist: "Brainfarm",
+        artwork: [
+          {
+            src: "https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg",
+            type: "image/jpeg",
+          },
+        ],
+      },
+      markers: {
+        enabled: true,
+        points: [
+          {
+            time: 40,
+            label: "first marker",
+          },
+          {
+            time: 80,
+            label: "second marker",
+          },
+          {
+            time: 120,
+            label: "<strong>third</strong> marker",
+          },
+        ],
+      },
+    });
+  },
+};
+</script>
